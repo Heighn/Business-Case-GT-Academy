07d6372e2f3c7e59571d3f7630f07f20a04cd780
hein.dehaan <hein.dehaan@laptop-Hein.getthere.tld>
2016-09-27 13:35:44 +0200

    Initiele bestanden en gitignore

---
+++ src/main/java/nl/getthere/BusinessCaseApplication.java
@@ -0,0 +1,12 @@
+package nl.getthere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCaseApplication.class, args);
+	}
+}
