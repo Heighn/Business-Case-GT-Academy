747cfe80241c2ade41b6e41ff44dd82b1e3702ef
Pieter Leijenaar <pieter.leijenaar@getthere.nl>
2016-09-28 16:26:28 +0200

    Recruiter field declaration + getters/setters

--- src/main/java/nl/getthere/users/Recruiter.java
+++ src/main/java/nl/getthere/users/Recruiter.java
@@ -1,7 +1,36 @@
 package nl.getthere.users;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Recruiter extends User {
+	
+	private String recruiterName;
+	private String recruiterPass;
+	private Long id;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+	    return id;
+	}
+
+	public String getRecruiterName() {
+		return recruiterName;
+	}
+
+	public void setRecruiterName(String recruiterName) {
+		this.recruiterName = recruiterName;
+	}
+
+	public String getRecruiterPass() {
+		return recruiterPass;
+	}
+
+	public void setRecruiterPass(String recruiterPass) {
+		this.recruiterPass = recruiterPass;
+	}
 }
