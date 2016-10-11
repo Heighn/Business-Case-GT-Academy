9d7a721571c167ce698171cd891b6e0c7a6f388b
Pieter Leijenaar <pieter.leijenaar@getthere.nl>
2016-10-11 16:47:14 +0200

    Full access to everything for everyone! :)

--- src/main/java/nl/getthere/model/WebSecurityConfig.java
+++ src/main/java/nl/getthere/model/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/styles.css", "/inloggen", "/images/*", "/student/registreren", "/recruiter/signUp").permitAll()
+                .antMatchers("/*", "/styles.css", "/inloggen", "/images/*", "/student/registreren", "/recruiter/signUp").permitAll()
                 .antMatchers("/student/*").hasAuthority("student")
                 .antMatchers("/recruiter/*").hasAuthority("student")
                 .anyRequest().authenticated()
